52d1b7a55f683ba01de5e41bb560d370b75f9aff
Evan Ronnei <etronnei@mtu.edu>
2016-01-20 23:31:13 -0500

    Create Actor.java

---
+++ src/Actor.java
@@ -0,0 +1,36 @@
+public class Actor {
+
+	protected int id;
+	protected int x;
+	protected int y;
+	protected int size;
+	protected int health;
+	protected int speed;
+	
+	// getters
+	public int getID() { return id; }
+	public int getX() { return x; }
+	public int getY() { return y; }
+	public int getSize() { return size; }
+	public int getHealth() { return health; }
+	public int getSpeed() { return speed; }
+	
+	// setters
+	public void setID(int id) { this.id = id; }
+	public void setX(int x) { this.x = x; }
+	public void setY(int y) { this.y = y; }
+	public void setSize(int size) { this.size = size; }
+	public void setHealth(int health) { this.health = health; }
+	public void setSpeed(int speed) { this.speed = speed; }
+	
+	public boolean isColliding(Actor a) {
+		// TODO collision code
+		return false;
+	}
+	
+	public String toString() {
+		String string = "<id: " + id + ", (x: " + x + ", y: " + y
+				+ "), size: " + size + ", health: " + health + ", speed: " + speed + ">";
+		return string;
+	}
+}
