12c00dc74a85d1f31f418cddcf0644cd2d402ab0
Evan Ronnei <etronnei@mtu.edu>
2016-01-21 16:15:38 -0500

    Reflect additions of Player and Arena classes

--- src/Darklight2.java
+++ src/Darklight2.java
@@ -10,109 +10,37 @@ import arcadia.Sound;
 
 public class Darklight2 extends Game {
 	
-	int xBoundL = 0 - WIDTH;
-	int xBoundR = WIDTH*2;
-	int yBoundU = 0 - HEIGHT;
-	int yBoundD = HEIGHT*2;
-	int xOffsetBorder = WIDTH/4;
-	int yOffsetBorder = HEIGHT/4;
-	int xOffset = 0;
-	int yOffset = 0;
-	
-	Actor player = new Actor(0, WIDTH/2, HEIGHT/2, 64, 10, 8);
+	Arena arena = new Arena();
+	Player player = new Player(0, WIDTH/2, HEIGHT/2, 64, 10, 8);
 	
 	int cooldown = 0;
 
 	@Override
 	public void tick(Graphics2D g, Input p1, Input p2, Sound s) {
 		
-		// background
-		g.setColor(Color.BLACK);
-		g.fillRect(0, 0, WIDTH, HEIGHT);
+		arena.draw(g);
 		
-		// floor grid
-		g.setColor(new Color(10, 10, 10));
-		for (int i = 0; i < (HEIGHT*3)/64; i++) {
-			if (i % 2 == 0) {
-				for (int j = 0; j < (WIDTH*3)/64; j += 2) {
-					g.fillRect(xBoundL + (64 * j) + xOffset, yBoundU + (64 * i) + yOffset, 64, 64);
-				}
-			} else {
-				for (int j = 1; j < (WIDTH*3)/64; j += 2) {
-					g.fillRect(xBoundL + (64 * j) + xOffset, yBoundU + (64 * i) + yOffset, 64, 64);
-				}
-			}
-		}
-		
-		// offset borders
-		g.setColor(Color.RED);
-		g.drawRect(0, 0, xOffsetBorder, HEIGHT);						// left
-		g.drawRect(WIDTH - xOffsetBorder, 0, xOffsetBorder, HEIGHT);	// right
-		g.drawRect(0, 0, WIDTH, yOffsetBorder);							// top
-		g.drawRect(0, HEIGHT - yOffsetBorder, WIDTH, yOffsetBorder);	// bottom
-		
-		// map boundaries
-		g.setColor(Color.YELLOW);
-		g.drawRect(xBoundL + xOffset, yBoundU + yOffset, WIDTH*3, HEIGHT*3);
-		
-		// static objects
-		g.setColor(Color.PINK);
-		g.fillRect(200 + xOffset, 200 + yOffset, 20, 20);
-		g.fillRect(800 + xOffset, 50 + yOffset, 20, 20);
-		g.fillRect(50 + xOffset, 300 + yOffset, 20, 20);
-		g.fillRect(500 + xOffset, 450 + yOffset, 20, 20);
-		g.fillRect(100 + xOffset, 250 + yOffset, 20, 20);
-		g.fillRect(650 + xOffset, 100 + yOffset, 20, 20);
-		
+//		// offset borders
+//		g.setColor(Color.RED);
+//		g.drawRect(0, 0, arena.xOffsetBorder, HEIGHT);								// left
+//		g.drawRect(WIDTH - arena.xOffsetBorder, 0, arena.xOffsetBorder, HEIGHT);	// right
+//		g.drawRect(0, 0, WIDTH, arena.yOffsetBorder);								// top
+//		g.drawRect(0, HEIGHT - arena.yOffsetBorder, WIDTH, arena.yOffsetBorder);	// bottom
+			
 		// player
-		g.setColor(Color.GREEN);
-		g.fillRect(player.getX() - (player.getSize()/2), player.getY() - (player.getSize()/2),
-			player.getSize(), player.getSize());
-		
-		// player movement
-		// left
-		if (p1.pressed(Button.L) && player.getX() > xBoundL + xOffset + (player.getSize()/2)) {
-			if (player.getX() > xOffsetBorder + (player.getSize()/2)) {
-				player.setX(player.getX() - player.getSpeed());
-			} else {
-				xOffset += player.getSpeed();
-			}
-		}
-		// right
-		if (p1.pressed(Button.R) && player.getX() < xBoundR + xOffset - (player.getSize()/2)) {
-			if (player.getX() < WIDTH - xOffsetBorder - (player.getSize()/2)) {
-				player.setX(player.getX() + player.getSpeed());
-			} else {
-				xOffset -= player.getSpeed();
-			}
-		}
-		// up
-		if (p1.pressed(Button.U) && player.getY() > yBoundU + yOffset + (player.getSize()/2)) {
-			if(player.getY() > yOffsetBorder + (player.getSize()/2)) {
-				player.setY(player.getY() - player.getSpeed());;
-			} else {
-				yOffset += player.getSpeed();
-			}
-		}
-		// down
-		if (p1.pressed(Button.D) && player.getY() < yBoundD + yOffset - (player.getSize()/2)) {
-			if (player.getY() < HEIGHT - yOffsetBorder - (player.getSize()/2)) {
-				player.setY(player.getY() + player.getSpeed());
-			} else {
-				yOffset -= player.getSpeed();
-			}
-		}
+		player.draw(g);
+		player.movement(p1, arena);
 		
 		// speed increasing
 		if (p1.pressed(Button.C) && cooldown == 0) {
-			player.setSpeed(player.getSpeed() * 2);
+			player.speed *= 2;
 			cooldown = 60;
 		}
 		if (cooldown > 0) {
 			cooldown -= 1;
 			
 			if (cooldown == 0) {
-				player.setSpeed(player.getSpeed() / 2);;
+				player.speed /= 2;
 			}
 		}
 		
@@ -122,7 +50,6 @@ public class Darklight2 extends Game {
 	@Override
 	public void reset() {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
