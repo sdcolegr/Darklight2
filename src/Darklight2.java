2b74abfc37d7491681b457b3f0d31896faca7d0d
Evan Ronnei <etronnei@mtu.edu>
2016-01-24 20:25:58 -0500

    added menus

--- src/Darklight2.java
+++ src/Darklight2.java
@@ -11,6 +11,10 @@ import arcadia.Sound;
 
 public class Darklight2 extends Game {
 	
+	int gameState = 0;
+	private HashMap<Button, Boolean> keyState = new HashMap<Button, Boolean>();
+	int button = 0;
+	
 	Arena arena = new Arena();
 	Player player = new Player(0, WIDTH/2, HEIGHT/2, 64, 10, 8);
 	Wave wave = new Wave();
@@ -20,48 +24,125 @@ public class Darklight2 extends Game {
 	@Override
 	public void tick(Graphics2D g, Input p1, Input p2, Sound s) {
 		
-		arena.draw(g);
-		
-//		// offset borders
-//		g.setColor(Color.RED);
-//		g.drawRect(0, 0, arena.xOffsetBorder, HEIGHT);								// left
-//		g.drawRect(WIDTH - arena.xOffsetBorder, 0, arena.xOffsetBorder, HEIGHT);	// right
-//		g.drawRect(0, 0, WIDTH, arena.yOffsetBorder);								// top
-//		g.drawRect(0, HEIGHT - arena.yOffsetBorder, WIDTH, arena.yOffsetBorder);	// bottom
+		// Menu
+		if (gameState == 0) {
 			
-		// player
-		player.draw(g);
-		player.movement(p1, arena);
-
-		wave.newWave();
-		wave.maintain(g, arena, player);
-
-		// speed increasing
-		if (p1.pressed(Button.C) && cooldown == 0) {
-			player.speed *= 2;
-			cooldown = 60;
+			// background
+			g.setColor(Color.BLACK);
+			g.fillRect(0, 0, WIDTH, HEIGHT);
+			
+			// title
+			g.setColor(Color.GREEN);
+			g.setFont(new Font("Arial", Font.BOLD, 100));
+			centerText("DARKLIGHT 2", g, WIDTH/2, 180);
+			
+			// selection
+			g.setColor(Color.RED);
+			if (button == 0) {
+				g.fillRect(WIDTH/2 - 160, 240, 320, 90);
+				if (justPressed(p1, Button.U) || justPressed(p1, Button.D)) {
+					button = 1;
+				}
+				if (justPressed(p1, Button.A) ||
+					justPressed(p1, Button.B) ||
+					justPressed(p1, Button.C)) {
+					gameState = 1;
+				}
+			} else {
+				g.fillRect(WIDTH/2 - 160, 340, 320, 90);
+				if (justPressed(p1, Button.U) || justPressed(p1, Button.D)) {
+					button = 0;
+				}
+				if (justPressed(p1, Button.A) ||
+						justPressed(p1, Button.B) ||
+						justPressed(p1, Button.C)) {
+						System.exit(0);
+					}
+			}
+			
+			// option boxes
+			g.setColor(Color.GREEN);
+			g.fillRect(WIDTH/2 - 150, 250, 300, 70);
+			g.fillRect(WIDTH/2 - 150, 350, 300, 70);
+			
+			// option box text
+			g.setColor(Color.BLACK);
+			g.setFont(new Font("Arial", Font.PLAIN, 30));
+			centerText("PLAY", g, WIDTH/2, 295);
+			centerText("EXIT", g, WIDTH/2, 395);
 		}
-		if (cooldown > 0) {
-			cooldown -= 1;
+		
+		// Game
+		if (gameState == 1) {
+			arena.draw(g);
 			
-			if (cooldown == 0) {
-				player.speed /= 2;
+//			// offset borders
+//			g.setColor(Color.RED);
+//			g.drawRect(0, 0, arena.xOffsetBorder, HEIGHT);								// left
+//			g.drawRect(WIDTH - arena.xOffsetBorder, 0, arena.xOffsetBorder, HEIGHT);	// right
+//			g.drawRect(0, 0, WIDTH, arena.yOffsetBorder);								// top
+//			g.drawRect(0, HEIGHT - arena.yOffsetBorder, WIDTH, arena.yOffsetBorder);	// bottom
+				
+			// player
+			player.draw(g);
+			player.movement(p1, arena);
+	
+			wave.newWave();
+			wave.maintain(g, arena, player);
+	
+			// speed increasing
+			if (p1.pressed(Button.C) && cooldown == 0) {
+				player.speed *= 2;
+				cooldown = 60;
+			}
+			if (cooldown > 0) {
+				cooldown -= 1;
+				
+				if (cooldown == 0) {
+					player.speed /= 2;
+				}
+			}
+			
+			// Collision
+			if( player.isColliding(enemy, arena)) {
+				player.health = player.health - 1;
+				
+				if (player.health == 0) {
+					System.out.println("gameover");
+				}
+	//			for (int i = 1; i < 2; i++){
+	//				System.out.println("hit " + i);
+	//			}
+			}
+	
+			g.dispose();
+		}
+		
+		updateKeyState(p1);
+	}
+	
+	private void centerText(String text, Graphics2D g, int x, int y) {
+
+		int width = g.getFontMetrics().stringWidth(text);
+		g.drawString(text, x - (width / 2), y);
+	}
+	
+	private void updateKeyState(Input p1) {
+
+		Button[] buttons = { Button.A, Button.B, Button.C, Button.U, Button.D,
+				Button.L, Button.R };
+
+		for (Button b : buttons) {
+			if (p1.pressed(b)) {
+				keyState.put(b, true);
+			} else {
+				keyState.remove(b);
 			}
 		}
-		
-		// Collision
-		if( player.isColliding(enemy, arena)) {
-			player.health = player.health - 1;
-			
-			if (player.health == 0) {
-				System.out.println("gameover");
-			}
-//			for (int i = 1; i < 2; i++){
-//				System.out.println("hit " + i);
-//			}
-		}
+	}
 
-		g.dispose();
+	private boolean justPressed(Input p1, Button source) {
+		return p1.pressed(source) && !keyState.containsKey(source);
 	}
 	
 	@Override
