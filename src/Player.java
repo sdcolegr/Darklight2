82486253d5e96d00e7c6e79dd8f304fda10e6a93
Evan Ronnei <etronnei@mtu.edu>
2016-01-26 18:14:26 -0500

    cleaned up isColliding

--- src/Player.java
+++ src/Player.java
@@ -65,12 +65,11 @@ public class Player extends Actor {
 	public boolean isColliding(Actor a, Arena arena) {
 		
 		if (x + (size/2) > a.x + arena.xOffset - (a.size/2) && 
-			x - (size/2) < a.x + arena.xOffset + (a.size/2)) {
+			x - (size/2) < a.x + arena.xOffset + (a.size/2) &&
+			y + (size/2) > a.y + arena.yOffset - (a.size/2) &&
+			y - (size/2) < a.y + arena.yOffset + (a.size/2)) {
 
-			if (y + (size/2) > a.y + arena.yOffset - (a.size/2) && 
-				y - (size/2) < a.y + arena.yOffset + (a.size/2)) {
-				return true;
-			}
+			return true;
 		}
 		return false;
 	}
