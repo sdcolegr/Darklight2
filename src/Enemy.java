d582500bd4459cb078b4403be078a9588d9e02dd
Codey Walker <codeyw@mtu.edu>
2016-01-26 18:52:01 -0500

    Update Enemy.java
    Better AI

--- src/Enemy.java
+++ src/Enemy.java
@@ -3,6 +3,10 @@ import java.awt.Graphics2D;
 
 public class Enemy extends Actor {
 
+	int cooldown = 0;
+	int randomX = 0;
+	int randomY = 0;
+	
 	public Enemy(int id, int x, int y, int size, int health, int speed) {
 		super(id, x, y, size, health, speed);
 	}
@@ -13,18 +17,55 @@ public class Enemy extends Actor {
 	}
 	
 	public void trackPlayer(Arena arena, Player player) {
-		
-		if (x + arena.xOffset > player.x) {
-			x -= speed;
+
+		if (x + arena.xOffset + (size / 2) + 256 > player.x &&
+			x + arena.xOffset - (size / 2) - 256 < player.x &&
+			y + arena.yOffset + (size / 2) + 256 > player.y &&
+			y + arena.yOffset - (size / 2) - 256 < player.y) {
+
+			if (x + arena.xOffset > player.x) {
+				x -= speed;
+			}
+			if (x + arena.xOffset < player.x) {
+				x += speed;
+			}
+			if (y + arena.yOffset > player.y) {
+				y -= speed;
+			}
+			if (y + arena.yOffset < player.y) {
+				y += speed;
+			}
+		} else if (cooldown < 30) {
+
+			
+			if (cooldown == 0) {
+				randomX = (int)(Math.random() * 3);
+				randomY = (int)(Math.random() * 3);
+			}
+			
+			if (randomX == 1) {
+				x += speed;
+			}
+			
+			else if (randomX == 2) {
+				x -= speed;
+			}
+
+			if (randomY == 1) {
+				y += speed;
+			}
+
+			else if (randomY == 2) {
+				y -= speed;
+			}
+			
+			cooldown ++;
 		}
-		if (x + arena.xOffset < player.x) {
-			x += speed;
-		}
-		if (y + arena.yOffset > player.y) {
-			y -= speed;
-		}
-		if (y + arena.yOffset < player.y) {
-			y += speed;
+		else {
+			cooldown ++;
+			if (cooldown == 90) {
+				cooldown = 0;
+			}
 		}
 	}
 	
