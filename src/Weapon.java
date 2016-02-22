65f099c8415e4da821b6283eaa0d61c4bc2c4e3e
Codey Walker <codeyw@mtu.edu>
2016-02-21 21:00:39 -0500

    Updated Weapon.java
    Added methods to account for weapon swapping.

--- src/Weapon.java
+++ src/Weapon.java
@@ -1,9 +1,9 @@
-
 public class Weapon {
 	
 	String name;
 	int damage;
 	int speed;
+	boolean pickedUp = false;
 	
 	//type of attack:
 	//slash, stab, etc
@@ -41,4 +41,41 @@ public class Weapon {
 			width = 192;
 		}
 	}	
+	
+	public boolean weaponPickedUp() {
+		return pickedUp;
+	}
+	
+	public void setPickedUp() {
+		pickedUp = true;
+	}
+	
+	public void setWeapon(String weaponName) {
+		name = weaponName;
+		if (name.equals("Short Sword")){
+			damage = 2;
+			//speed = ;
+			atType = 0;
+			length = 64;
+			width = 96;
+		} else if (name.equals("Spear")){
+			damage = 5;
+			//speed = ;
+			atType = 1;
+			length = 128;
+			width = 16;
+		} else if (name.equals("Greatsword")){
+			damage = 50;
+			//speed = ;
+			atType = 2;
+			length = 96;
+			width = 192;
+		}
+	}
+	
+	public String getWeapon() {
+		return name;
+	}
+	
+	
 }
