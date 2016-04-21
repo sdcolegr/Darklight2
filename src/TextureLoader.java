c6e4dfd13c3d4e2c3e4984821332685fb6f3fcf4
Evan Ronnei <etronnei@mtu.edu>
2016-04-21 09:51:17 -0400

    Update TextureLoader.java

--- src/TextureLoader.java
+++ src/TextureLoader.java
@@ -12,6 +12,15 @@ public class TextureLoader {
 	public static Image mossSkull;
 	public static Image mossHelm;
 	public static Image guy;
+	public static Image topLeftWall;
+	public static Image topWall;
+	public static Image topRightWall;
+	public static Image leftWall;
+	public static Image rightWall;
+	public static Image bottomLeftWall;
+	public static Image bottomWall;
+	public static Image bottomRightWall;
+	public static Image light;
 
 	public static void load() {
 
@@ -23,6 +32,15 @@ public class TextureLoader {
 			mossSkull = ImageIO.read(TextureLoader.class.getResource("Resources/Textures/Tiles/mossSkull.png"));
 			mossHelm = ImageIO.read(TextureLoader.class.getResource("Resources/Textures/Tiles/mossHelm.png"));
 			guy = ImageIO.read(TextureLoader.class.getResource("Resources/Textures/Tiles/guy.png"));
+			topLeftWall = ImageIO.read(TextureLoader.class.getResource("Resources/Textures/Walls/topLeftWall.png"));
+			topWall = ImageIO.read(TextureLoader.class.getResource("Resources/Textures/Walls/topWall.png"));
+			topRightWall = ImageIO.read(TextureLoader.class.getResource("Resources/Textures/Walls/topRightWall.png"));
+			leftWall = ImageIO.read(TextureLoader.class.getResource("Resources/Textures/Walls/leftWall.png"));
+			rightWall = ImageIO.read(TextureLoader.class.getResource("Resources/Textures/Walls/rightWall.png"));
+			bottomLeftWall = ImageIO.read(TextureLoader.class.getResource("Resources/Textures/Walls/bottomLeftWall.png"));
+			bottomWall = ImageIO.read(TextureLoader.class.getResource("Resources/Textures/Walls/bottomWall.png"));
+			bottomRightWall = ImageIO.read(TextureLoader.class.getResource("Resources/Textures/Walls/bottomRightWall.png"));
+			light = ImageIO.read(TextureLoader.class.getResource("Resources/Textures/Misc/light.png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
